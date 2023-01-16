3eb66c8268e7de90aa20fa62956ba974e688d02b
tosined <o.dada@diasprex.com>
2023-01-15 21:06:02 -0800

    changes to viewproposal page and created sponcoProposalcard2

---
+++ src/app/modules/proposals/components/SponsorProposalCard2.tsx
@@ -0,0 +1,150 @@
+//@ts-nocheck
+import clsx from 'clsx'
+import {FC} from 'react'
+import {useNavigate, Link} from 'react-router-dom'
+import {Proposal} from '../../apps/admin-mgt-apps/proposal-management/props-list/core/_models'
+import {toAbsoluteUrl} from '../../../../_metronic/helpers'
+
+type Props = {
+  proposal?: Proposal
+  dashboard?: boolean
+}
+const SponsorProposalCard2: FC<Props> = ({dashboard, proposal}) => {
+  const badgeColor =
+    proposal?.status === 'new'
+      ? 'info'
+      : proposal?.status === 'selected'
+      ? 'success'
+      : proposal?.status === 'draft'
+      ? 'gray-800'
+      : proposal?.status === 'declined'
+      ? 'danger'
+      : proposal?.status === 'pending'
+      ? 'gray-600'
+      : proposal?.status === 'completed'
+      ? 'gray-800'
+      : proposal?.status === 'active'
+      ? 'primary'
+      : 'warning'
+  const navigate = useNavigate()
+
+  return (
+    <div className='KTCard mb-5'>
+      <div className='card shadow-sm mb-6 mb-xl-9'>
+        <div className={`card-header ribbon bg-gray-400 ribbon-end ribbon-clip`}>
+          <div className='d-flex align-items-center mb-1'>
+            <Link to={`#`} className='text-gray-800 text-hover-primary fs-2 fw-bold me-3'>
+              {proposal?.enablerName}
+            </Link>
+            <span className='symbol symbol-30px w-30px bg-light me-2'>
+              <img
+                src={toAbsoluteUrl(`/media/flags/${proposal?.country?.toLowerCase()}.svg`)}
+                className='fs-6 fw-bold'
+                alt={proposal?.country}
+                data-toggle='tooltips'
+                title={proposal?.country}
+                data-bs-placement='bottom'
+              />
+            </span>
+            {/* <span
+              className={`badge ${
+                proposal?.status ? 'badge-light-success' : 'badge-light-danger'
+              } fw-bolder me-auto py-3`}
+            >
+              {proposal?.status}
+            </span> */}
+          </div>
+          <div className='ribbon-label text-capitalize'>
+            {proposal?.status}
+            <span className={`ribbon-inner bg-${badgeColor}`}></span>
+          </div>
+        </div>
+
+        <div className='card-body pt-9 pb-0'>
+          <div className='d-flex flex-wrap flex-sm-nowrap mb-6'>
+            <div
+              className={`d-flex flex-center flex-shrink-0 bg-light-${badgeColor} rounded w-50px h-50px w-lg-100px h-lg-100px me-7 mb-4`}
+            >
+              {proposal?.thumbnail === '' ? (
+                <div
+                  className={clsx(
+                    'd-flex symbol-label mw-100 align-items-center justify-content-center fs-1 rounded',
+                    `bg-light-${badgeColor}`,
+                    ` text-capitalize text-${badgeColor}`
+                  )}
+                >
+                  {proposal?.country}
+                </div>
+              ) : (
+                <img className='d-block mw-100 rounded' src={proposal?.thumbnail} alt='oppsthumb' />
+              )}
+            </div>
+            <div className='flex-grow-1'>
+              <div className='d-flex justify-content-between align-items-start flex-wrap mb-2'>
+                <div className='d-flex flex-column'>
+                  <Link
+                    to={`/proposals/${proposal?.opportunityObject?.uuid}/${proposal?.enablerUserId}`}
+                    className='d-flex flex-wrap fw-semibold mb-4 fs-5 text-dark'
+                  >
+                    TITLE: {proposal?.title}
+                  </Link>
+                </div>
+                <div className={`col-lg-3 d-flex align-items-start justify-content-end`}>
+                  {proposal?.status !== 'active' && proposal?.status !== 'completed' && dashboard && (
+                    <>
+                      <button
+                        className='btn btn-sm btn-light btn-active-light-primary dropdown-toggle'
+                        type='button'
+                        data-bs-toggle='dropdown'
+                        aria-haspopup='true'
+                        aria-expanded='false'
+                      >
+                        Actions
+                      </button>
+                      <div
+                        className='dropdown-menu menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-6 w-125px py-4'
+                        data-kt-menu='true'
+                      >
+                        <div className='dropdown-item px-3'>
+                          <a
+                            className='menu-link px-3'
+                            onClick={() => {
+                              navigate(
+                                `/proposals/${proposal?.opportunityObject?.uuid}/${proposal?.enablerUserId}`
+                              )
+                            }}
+                          >
+                            View
+                          </a>
+                        </div>
+
+                        {/* {(opp?.status === 'new' || opp?.status === 'pending') && (
+                          <div className='dropdown-item px-3'>
+                            <a
+                              className='menu-link px-3'
+                              onClick={() => {
+                                handleWithdrawOpp(opp?.uuid, 'withdrawn')
+                              }}
+                            >
+                              Withdraw
+                            </a>
+                          </div>
+                        )} */}
+                      </div>
+                    </>
+                  )}
+                </div>
+              </div>
+              <div className='d-flex flex-wrap justify-content-start'>
+                <div className='d-flex flex-wrap'>
+                  SUMMARY: {proposal?.summary?.slice(0, 100)}...
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default SponsorProposalCard2
--- src/app/modules/proposals/components/viewproposal.tsx
+++ src/app/modules/proposals/components/viewproposal.tsx
@@ -6,15 +6,16 @@ import {Row, Col, Button, Card, notification, Tooltip} from 'antd'
 import {StarOutlined, ShareAltOutlined} from '@ant-design/icons'
 import {toAbsoluteUrl} from '../../../../_metronic/helpers'
 import {Proposal} from '../../apps/admin-mgt-apps/proposal-management/props-list/core/_models'
-import * as proposal from '../../../modules/proposals/redux/ProposalRedux'
+import * as proposal from '../redux/ProposalRedux'
 import {RootState} from '../../../../setup'
-import {useParams} from 'react-router-dom'
+import {Link, useParams} from 'react-router-dom'
 import {ListLoading} from '../../apps/admin-mgt-apps/core/loading/ListLoading'
 
 const mapState = (state: RootState) => ({proposal: state.proposals})
 const connector = connect(mapState, proposal.actions)
 type PropsFromRedux = ConnectedProps<typeof connector>
-
+const userType = localStorage.getItem('userType')
+console.log('UserType', userType)
 const ViewProposal: FC<PropsFromRedux> = (props) => {
   const {oppid: oppId, enablerid: enablerId} = useParams()
   const dispatch = useDispatch()
@@ -40,116 +41,212 @@ const ViewProposal: FC<PropsFromRedux> = (props) => {
         <ListLoading />
       ) : (
         <>
-          <div className='card shadow-sm mb-6 mb-xl-9'>
-            <div className='card-body pt-9 pb-0'>
-              <div className='d-flex flex-wrap flex-sm-nowrap mb-6'>
-                <div
-                  className={`d-flex flex-center flex-shrink-0 bg-light-${badgeColor} rounded w-100px h-100px w-lg-150px h-lg-150px me-7 mb-4`}
-                >
-                  {propData?.opportunityObject?.thumbnail === '' ? (
-                    <div
-                      className={clsx(
-                        'd-flex symbol-label mw-100 h-100px h-lg-150px align-items-center justify-content-center fs-1 rounded',
-                        `bg-light-${badgeColor}`,
-                        ` text-capitalize text-${badgeColor}`
-                      )}
-                    >
-                      {propData?.opportunityObject?.country}
-                    </div>
-                  ) : (
-                    <img
-                      className='d-block mw-100 rounded'
-                      src={propData?.opportunityObject?.thumbnail}
-                      alt='oppsthumb'
-                    />
-                  )}
-                </div>
-                <div className='flex-grow-1'>
-                  <div className='d-flex justify-content-between align-items-start flex-wrap mb-2'>
-                    <div className='d-flex flex-column'>
-                      <div className='d-flex align-items-center mb-1'>
-                        <a href='#' className='text-gray-800 text-hover-primary fs-2 fw-bold me-3'>
-                          {propData?.opportunityObject?.sponsorName}
-                        </a>
-                        <span className='symbol symbol-30px w-30px bg-light me-2'>
-                          <img
-                            src={toAbsoluteUrl(
-                              `/media/flags/${propData?.opportunityObject?.country?.toLowerCase()}.svg`
-                            )}
-                            className='fs-6 fw-bold'
-                            alt={propData?.opportunityObject?.country}
-                            data-toggle='tooltips'
-                            title={propData?.opportunityObject?.country}
-                            data-bs-placement='bottom'
-                          />
-                        </span>
-                        <span className={`badge badge-light-${badgeColor} me-auto`}>
-                          {propData?.opportunityObject?.open ? 'Open' : 'Closed'}
-                        </span>
+          {userType === 'enabler' && (
+            <div className='card shadow-sm mb-6 mb-xl-9'>
+              <div className='card-body pt-9 pb-0'>
+                <div className='d-flex flex-wrap flex-sm-nowrap mb-6'>
+                  <div
+                    className={`d-flex flex-center flex-shrink-0 bg-light-${badgeColor} rounded w-100px h-100px w-lg-150px h-lg-150px me-7 mb-4`}
+                  >
+                    {propData?.opportunityObject?.thumbnail === '' ? (
+                      <div
+                        className={clsx(
+                          'd-flex symbol-label mw-100 h-100px h-lg-150px align-items-center justify-content-center fs-1 rounded',
+                          `bg-light-${badgeColor}`,
+                          ` text-capitalize text-${badgeColor}`
+                        )}
+                      >
+                        {propData?.opportunityObject?.country}
                       </div>
-
-                      <div className='d-flex flex-wrap fw-semibold mb-4 fs-5 text-gray-400'>
-                        Title: {propData?.opportunityObject?.title}
-                      </div>
-                    </div>
-                    <div className='d-flex mb-4'>
-                      {/* Click Follow increases following parameter by +1 text changed to unfollow if Enabler is already following */}
-                      <button type='button' className='btn btn-sm btn-success me-3' disabled={true}>
-                        Follow
-                      </button>
-                    </div>
+                    ) : (
+                      <img
+                        className='d-block mw-100 rounded'
+                        src={propData?.opportunityObject?.thumbnail}
+                        alt='oppsthumb'
+                      />
+                    )}
                   </div>
-                  <div className='d-flex flex-wrap justify-content-start'>
-                    <div className='d-flex flex-wrap'>
-                      <div className='border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 me-6 mb-3'>
-                        <div className='d-flex align-items-center'>
-                          <div className='fs-4 fw-bold'>
-                            {moment(propData?.opportunityObject?.duedate).format('MMM Do, YYYY')}
-                          </div>
+                  <div className='flex-grow-1'>
+                    <div className='d-flex justify-content-between align-items-start flex-wrap mb-2'>
+                      <div className='d-flex flex-column'>
+                        <div className='d-flex align-items-center mb-1'>
+                          <a
+                            href='#'
+                            className='text-gray-800 text-hover-primary fs-2 fw-bold me-3'
+                          >
+                            {propData?.opportunityObject?.sponsorName}
+                          </a>
+                          <span className='symbol symbol-30px w-30px bg-light me-2'>
+                            <img
+                              src={toAbsoluteUrl(
+                                `/media/flags/${propData?.opportunityObject?.country?.toLowerCase()}.svg`
+                              )}
+                              className='fs-6 fw-bold'
+                              alt={propData?.opportunityObject?.country}
+                              data-toggle='tooltips'
+                              title={propData?.opportunityObject?.country}
+                              data-bs-placement='bottom'
+                            />
+                          </span>
+                          <span className={`badge badge-light-${badgeColor} me-auto`}>
+                            {propData?.opportunityObject?.open ? 'Open' : 'Closed'}
+                          </span>
                         </div>
 
-                        <div className='fw-semibold fs-6 text-gray-400'>Due Date</div>
+                        <div className='d-flex flex-wrap fw-semibold mb-4 fs-5 text-gray-400'>
+                          Title: {propData?.opportunityObject?.title}
+                        </div>
                       </div>
-                      <div className='border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 me-6 mb-3'>
-                        <div className='d-flex align-items-center'>
-                          <div className='fs-4 fw-bold'>
-                            {propData?.opportunityObject?.dealtype?.map(
-                              (deal: string, index: number) =>
-                                index !== dealTypeLength ? (
-                                  <span className='me-1'>{deal} |</span>
-                                ) : (
-                                  <span className='me-1'>{deal}</span>
-                                )
-                            )}
-                          </div>
-                        </div>
-
-                        <div className='fw-semibold fs-6 text-gray-400'>Deal Type</div>
+                      <div className='d-flex mb-4'>
+                        {/* Click Follow increases following parameter by +1 text changed to unfollow if Enabler is already following */}
+                        <button
+                          type='button'
+                          className='btn btn-sm btn-success me-3'
+                          disabled={true}
+                        >
+                          Follow
+                        </button>
                       </div>
-                      <div className='border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 me-6 mb-3'>
-                        <div className='d-flex align-items-center'>
-                          <div className='fs-4 fw-bold'>
-                            {propData?.opportunityObject?.showedinterest?.length}
+                    </div>
+                    <div className='d-flex flex-wrap justify-content-start'>
+                      <div className='d-flex flex-wrap'>
+                        <div className='border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 me-6 mb-3'>
+                          <div className='d-flex align-items-center'>
+                            <div className='fs-4 fw-bold'>
+                              {moment(propData?.opportunityObject?.duedate).format('MMM Do, YYYY')}
+                            </div>
                           </div>
-                        </div>
 
-                        <div className='fw-semibold fs-6 text-gray-400'>Show of Interest</div>
-                      </div>
-                      <div className='border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 me-6 mb-3'>
-                        <div className='d-flex align-items-center'>
-                          <div className='fs-4 fw-bold'>
-                            {propData?.opportunityObject?.following?.length}
+                          <div className='fw-semibold fs-6 text-gray-400'>Due Date</div>
+                        </div>
+                        <div className='border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 me-6 mb-3'>
+                          <div className='d-flex align-items-center'>
+                            <div className='fs-4 fw-bold'>
+                              {propData?.opportunityObject?.dealtype?.map(
+                                (deal: string, index: number) =>
+                                  index !== dealTypeLength ? (
+                                    <span className='me-1'>{deal} |</span>
+                                  ) : (
+                                    <span className='me-1'>{deal}</span>
+                                  )
+                              )}
+                            </div>
                           </div>
-                        </div>
 
-                        <div className='fw-semibold fs-6 text-gray-400'>Following</div>
+                          <div className='fw-semibold fs-6 text-gray-400'>Deal Type</div>
+                        </div>
+                        <div className='border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 me-6 mb-3'>
+                          <div className='d-flex align-items-center'>
+                            <div className='fs-4 fw-bold'>
+                              {propData?.opportunityObject?.showedinterest?.length}
+                            </div>
+                          </div>
+
+                          <div className='fw-semibold fs-6 text-gray-400'>Show of Interest</div>
+                        </div>
+                        <div className='border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 me-6 mb-3'>
+                          <div className='d-flex align-items-center'>
+                            <div className='fs-4 fw-bold'>
+                              {propData?.opportunityObject?.following?.length}
+                            </div>
+                          </div>
+
+                          <div className='fw-semibold fs-6 text-gray-400'>Following</div>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+          )}
+
+          {userType === 'sponsor' && (
+            <div className='card shadow-sm mb-6 mb-xl-9'>
+              <div className='card-body pt-9 pb-0'>
+                <div className='d-flex flex-wrap flex-sm-nowrap mb-6'>
+                  <div
+                    className={`d-flex flex-center flex-shrink-0 bg-light-${badgeColor} rounded w-100px h-100px w-lg-150px h-lg-150px me-7 mb-4`}
+                  >
+                    {propData?.thumbnail === '' ? (
+                      <div
+                        className={clsx(
+                          'd-flex symbol-label mw-100 h-100px h-lg-150px align-items-center justify-content-center fs-1 rounded',
+                          `bg-light-${badgeColor}`,
+                          ` text-capitalize text-${badgeColor}`
+                        )}
+                      >
+                        {propData?.country}
+                      </div>
+                    ) : (
+                      <img
+                        className='d-block mw-100 rounded'
+                        src={propData?.thumbnail}
+                        alt='oppsthumb'
+                      />
+                    )}
+                  </div>
+                  <div className='flex-grow-1'>
+                    <div className='d-flex justify-content-between align-items-start flex-wrap mb-2'>
+                      <div className='d-flex flex-column'>
+                        <div className='d-flex align-items-center mb-1'>
+                          <a
+                            href='#'
+                            className='text-gray-800 text-hover-primary fs-2 fw-bold me-3'
+                          >
+                            {propData?.enablerName}
+                          </a>
+                          <span className='symbol symbol-30px w-30px bg-light me-2'>
+                            <img
+                              src={toAbsoluteUrl(
+                                `/media/flags/${propData?.country?.toLowerCase()}.svg`
+                              )}
+                              className='fs-6 fw-bold'
+                              alt={propData?.opportunityObject?.country}
+                              data-toggle='tooltips'
+                              title={propData?.opportunityObject?.country}
+                              data-bs-placement='bottom'
+                            />
+                          </span>
+                          <span className={`badge badge-light-${badgeColor} me-auto`}>
+                            {propData?.status}
+                          </span>
+                        </div>
+
+                        <div className='d-flex flex-wrap fw-semibold mb-4 fs-5 text-gray-400'>
+                          Title: {propData?.title}
+                        </div>
+                      </div>
+                      <div className='d-flex mb-4'>
+                        {/* Click Follow increases following parameter by +1 text changed to unfollow if Enabler is already following */}
+                        <Link
+                          to='/sponsor/props_review/proposals'
+                          className='btn btn-sm btn-secondary me-3'
+                        >
+                          Proposal Table
+                        </Link>
+                      </div>
+                    </div>
+                    {/* <div className='d-flex flex-wrap justify-content-start'>
+                      <div className='d-flex flex-wrap'>
+                        <div className='border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 me-6 mb-3'>
+                          <div className='d-flex align-items-center'>
+                            <div className='fs-4 fw-bold'>
+                              {moment(propData?.opportunityObject?.duedate).format('MMM Do, YYYY')}
+                            </div>
+                          </div>
+
+                          <div className='fw-semibold fs-6 text-gray-400'>Due Date</div>
+                        </div>
+                      
+                       
+                      </div>
+                    </div> */}
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
 
           <div className='card mb-5 mb-xl-10'>
             <div
