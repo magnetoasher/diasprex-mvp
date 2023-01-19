e2e26e4082b52de2185175520ef336afee7844f9
magnetoasher <xcaliburtriam@gmail.com>
2023-01-19 10:14:10 -0500

    integrated: api for approve/delete a feedback on Admin side

--- src/app/pages/dashboard/NewDashboardPage.tsx
+++ src/app/pages/dashboard/NewDashboardPage.tsx
@@ -86,22 +86,6 @@ const NewDashboardPage: React.FC<PropsFromRedux> = (props) => {
   }, [])
 
   useEffect(() => {
-    if (userType === 'sponsor') {
-      const filteredOpps = props.opps.opps?.data?.filter((obj: Opps) => {
-        return obj.status === 'new' ||
-        obj.status === 'published' ||
-        obj.status === 'accepted' ||
-        obj.status === 'accepted with revision' ||
-        obj.status === 'not accepted' ||
-        obj.status === 'withdrawn'
-      })
-      setRecentOpps(filteredOpps)
-    } else {
-      setRecentOpps(props.opps.opps?.data)
-    }
-  }, [props.opps])
-
-  useEffect(() => {
     setRecentProps(props.proposals.proposals?.data)
   }, [props.proposals])
 
